d2fee8c4dc319230e5dd3f0815db401d55dabd56
Liandrel <109763767+Liandrel@users.noreply.github.com>
2022-10-26 21:49:34 +0200

    Delete order Page
    Added Delete order page and delete option in Display page

---
+++ RPDemo/Pages/Orders/Delete.cshtml.cs
@@ -0,0 +1,32 @@
+using DataLibrary.Data;
+using DataLibrary.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RPDemo.Pages.Orders
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly IOrderData _orderData;
+
+        [BindProperty(SupportsGet = true)]
+        public int Id { get; set; }
+
+        public OrderModel  Order { get; set; }
+        public DeleteModel(IOrderData orderData)
+        {
+            _orderData = orderData;
+        }
+        public async Task OnGet()
+        {
+            Order = await _orderData.GetOrderById(Id);
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+            _orderData.DeleteOrder(Id);
+
+            return RedirectToPage("./Create");
+        }
+    }
+}
